90289c2be0f53d8bca0bbf629a182280b2e09347
Dan Jackson <danjacksonis@googlemail.com>
2024-02-10 14:10:59 +0100

    Added bricks and physics!

--- src/entry_point.js
+++ src/entry_point.js
@@ -4,18 +4,40 @@ import Phaser from 'phaser/dist/phaser.js'
 import SawfishSpritesheet from 'Assets/textures/sawfish.png'
 import SawfishData from 'Assets/textures/sawfish.json'
 import Jingle from 'Assets/sounds//music//phaser-jingle.wav'
+import Brick from 'Assets/textures/brick.png'
 
 class Example extends Phaser.Scene {
   preload () {
     this.load.aseprite('sawfish', SawfishSpritesheet, SawfishData)
     this.load.audio('jingle', [Jingle])
+    this.load.image('brick', Brick)
   }
 
   create () {
     this.anims.createFromAseprite('sawfish')
-    this.add.text(50, 50, 'Set Phaser to stun; you\'re good to go!', { fontFamily: 'sans' })
-    const ssprite = this.add.sprite(200, 150, 'sawfish').play({ key: 'spin', repeat: -1 })// eslint-disable-line no-unused-vars
-    this.add.text(50, 250, 'No sound? Try clicking the panel.', { fontFamily: 'sans' })
+    this.add.text(10, 50, 'CAPTAIN SAWFISH SAVES THE F**KING WORLD', { fontFamily: 'mono' })
+    const ssprite = this.physics.add.sprite(200, 150, 'sawfish').play({ key: 'spin', repeat: -1 })// eslint-disable-line no-unused-vars
+
+    const bricks = this.physics.add.staticGroup()
+    const brickConfigs = []
+    for (let j = 0; j < 16; j++) {
+      for (let i = 0; i < Math.floor(400 / 12) + 1; i++) {
+        const brick = {
+          key: 'brick',
+          setXY: {
+            x: (i * 12) - (j % 2 ? 0 : 6),
+            y: 220 + (6 * j)
+          },
+          setOrigin: {
+            x: 0,
+            y: 0
+          }
+        }
+        brickConfigs.push(brick)
+      }
+    }
+    this.physics.add.collider(ssprite, bricks)
+    bricks.createMultiple(brickConfigs)
     const music = this.sound.add('jingle')
     music.play()
   }
@@ -27,7 +49,14 @@ const config = {
   height: 300,
   scene: Example,
   pixelArt: true,
-  zoom: 3
+  zoom: 3,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  }
 }
 
 const game = new Phaser.Game(config) // eslint-disable-line no-unused-vars
