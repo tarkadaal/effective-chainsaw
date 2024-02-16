415bb185f5ffd3c53404e4b40a012d2e60603de3
Dan Jackson <danjacksonis@googlemail.com>
2024-02-16 13:26:22 +0100

    First stab at physics. Needs lots of tweaking

--- src/entry_point.js
+++ src/entry_point.js
@@ -20,30 +20,54 @@ class Example extends Phaser.Scene {
     this.anims.createFromAseprite('sawfish')
     // this.add.text(10, 50, 'CAPTAIN SAWFISH SAVES THE F**KING WORLD', { fontFamily: 'mono' })
     this.add.bitmapText(35, 70, 'coolfont', 'CAPTAIN SAWFISH SAVES THE F**KING WORLD!')
-    const ssprite = this.physics.add.sprite(200, 150, 'sawfish').play({ key: 'eyebrows', repeat: -1 })// eslint-disable-line no-unused-vars
+    const sawfishShape = [[
+      { x: 0, y: 0 },
+      { x: 53, y: 0 },
+      { x: 56, y: 2 },
+      { x: 59, y: 4 },
+      { x: 59, y: 6 },
+      { x: 56, y: 7 },
+      { x: 53, y: 9 },
+      { x: 0, y: 9 }
+    ]]
+    this.sawfish = this.matter.add.sprite(50, 150, 'sawfish', null, {
+      shape: {
+        type: 'fromVerts',
+        verts: sawfishShape
+      },
+      friction: 0.05,
+      frictionStatic: 0.2,
+      render: { sprite: { xOffset: 0, yOffset: 0.5 } }
+    }
+    )
+    this.sawfish.play({ key: 'eyebrows', repeat: -1 })// eslint-disable-line no-unused-vars
 
-    const bricks = this.physics.add.staticGroup()
-    const brickConfigs = []
     for (let j = 0; j < 16; j++) {
       for (let i = 0; i < Math.floor(400 / 12) + 1; i++) {
-        const brick = {
-          key: 'brick',
-          setXY: {
-            x: (i * 12) - (j % 2 ? 0 : 6),
-            y: 220 + (6 * j)
-          },
-          setOrigin: {
-            x: 0,
-            y: 0
-          }
-        }
-        brickConfigs.push(brick)
+        this.add.sprite(
+          (i * 12) - (j % 2 ? 0 : 6),
+          220 + (6 * j),
+          'brick'
+        )
       }
     }
-    this.physics.add.collider(ssprite, bricks)
-    bricks.createMultiple(brickConfigs)
+    this.matter.add.rectangle(200, 266, 600, 100, { isStatic: true })
     const music = this.sound.add('jingle')
+
     music.play()
+    this.cursors = this.input.keyboard.createCursorKeys()
+  }
+
+  update () {
+    const spinSpeed = this.sawfish.getAngularVelocity()
+    const velocity = this.sawfish.getVelocity()
+    if (this.cursors.right.isDown) {
+      this.sawfish.setAngularVelocity(spinSpeed + (0.009 * velocity.x))
+      this.sawfish.setVelocity(velocity.x + 1.7)
+    } else {
+      //      this.sawfish.setAngularVelocity(spinSpeed - 0.1)
+
+    }
   }
 }
 
@@ -55,10 +79,10 @@ const config = {
   pixelArt: true,
   zoom: 3,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 300 },
-      debug: false
+    default: 'matter',
+    matter: {
+      gravity: { y: 0.8 },
+      debug: true
     }
   }
 }
